58a980c4e572eb2265f65c25682180d0e06494b9
Haider Malik <haidermalik504@gmail.com>
2018-04-19 12:37:08 +0500

    setup prettier

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.json({
-        msg: 'Welcome to Invoice builder backend'
-    })
+  res.json({
+    msg: 'Welcome to Invoice builder backend'
+  });
 });
 app.listen(PORT, () => {
-    console.log(`Server is running at PORT ${PORT}`);
+  console.log(`Server is running at PORT ${PORT}`);
 });
