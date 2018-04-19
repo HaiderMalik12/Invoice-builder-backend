848c2226ea37c6be8823b24d3c464c27dff31495
Haider Malik <haidermalik504@gmail.com>
2018-04-19 14:34:17 +0500

    creating custom middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use((req, res, next) => {
+  console.log('Time: %d', Date.now());
+
+  next();
+});
 app.get('/', (req, res) => {
   res.json({
     msg: 'Welcome to Invoice builder backend',
