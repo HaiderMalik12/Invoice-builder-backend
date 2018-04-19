9e674aea8a0a7c5b650fa4a620420cda46f78980
Haider Malik <haidermalik504@gmail.com>
2018-04-19 12:46:32 +0500

    eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    msg: 'Welcome to Invoice builder backend'
+    msg: 'Welcome to Invoice builder backend',
   });
 });
 app.listen(PORT, () => {
