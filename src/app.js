2b4239c064df8891c43b1e9b2e98b24be32252cc
Haider Malik <haidermalik504@gmail.com>
2018-04-19 12:59:31 +0500

    create invoice listing enpoint

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     msg: 'Welcome to Invoice builder backend',
   });
 });
+const invoices = [
+  { _id: '123123', item: 'Amazon Product', qty: 10, date: new Date() },
+  { _id: '223123', item: 'GOogle Product', qty: 10, date: new Date() },
+  { _id: '323123', item: 'Linked Product', qty: 10, date: new Date() },
+];
+app.get('/invoices', (req, res) => {
+  res.json(invoices);
+});
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
