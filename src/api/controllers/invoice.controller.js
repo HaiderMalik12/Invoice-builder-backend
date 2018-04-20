234a3244f2a181b8a0a7d12741d651be7f3e257a
Haider Malik <haidermalik504@gmail.com>
2018-04-20 21:45:02 +0500

    romved array

--- src/api/controllers/invoice.controller.js
+++ src/api/controllers/invoice.controller.js
@@ -1,10 +1,7 @@
-const invoices = [
-  { _id: '123123', item: 'Amazon Product', qty: 10, date: new Date() },
-  { _id: '223123', item: 'GOogle Product', qty: 10, date: new Date() },
-  { _id: '323123', item: 'Linked Product', qty: 10, date: new Date() },
-];
+import Invoice from '../models/invoice.model';
+
 export default {
   findAll(req, res, next) {
-    res.json(invoices);
+    Invoice.find().then(invoices => res.json(invoices));
   },
 };
