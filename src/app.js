8deb27c35b91631200eccfaa4acc2f2e18a23ec9
Haider Malik <haidermalik504@gmail.com>
2018-04-24 11:55:06 +0500

    handle cors issue on backend side

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import logger from 'morgan';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import { router } from './config/routes';
 import swaggerDocument from './config/swagger.json';
@@ -13,6 +14,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 app.use(logger('dev'));
 app.use(
   '/api-docs',
