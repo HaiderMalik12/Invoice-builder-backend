cbe2d683e19a7a41b3653fdeb8800619c1e0d0cc
Haider Malik <haidermalik504@gmail.com>
2018-05-04 18:39:46 +0500

    create client using async/await

--- src/api/resources/client/client.controller.js
+++ src/api/resources/client/client.controller.js
@@ -1,12 +1,18 @@
-import { BAD_REQUEST } from 'http-status-codes';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import clientService from './client.service';
+import Client from './client.model';
 
 export default {
   async create(req, res) {
-    const { value, error } = clientService.validateCreateSchema(req.body);
-    if (error && error.details) {
-      return res.status(BAD_REQUEST).json(error);
+    try {
+      const { value, error } = clientService.validateCreateSchema(req.body);
+      if (error && error.details) {
+        return res.status(BAD_REQUEST).json(error);
+      }
+      const client = await Client.create(value);
+      return res.json(client);
+    } catch (err) {
+      res.status(INTERNAL_SERVER_ERROR).json(err);
     }
-    return res.json(value);
   },
 };
