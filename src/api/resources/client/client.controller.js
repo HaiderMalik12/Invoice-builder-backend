8ef630f881fcd911c4c23fe75ae529644c3da8e1
Haider Malik <haidermalik504@gmail.com>
2018-05-04 18:37:22 +0500

    validate schema for create client

--- src/api/resources/client/client.controller.js
+++ src/api/resources/client/client.controller.js
@@ -1,5 +1,12 @@
+import { BAD_REQUEST } from 'http-status-codes';
+import clientService from './client.service';
+
 export default {
   async create(req, res) {
-    return res.json({ msg: 'Create Client' });
+    const { value, error } = clientService.validateCreateSchema(req.body);
+    if (error && error.details) {
+      return res.status(BAD_REQUEST).json(error);
+    }
+    return res.json(value);
   },
 };
---
+++ src/api/resources/client/client.service.js
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+export default {
+  validateCreateSchema(body) {
+    const schema = Joi.object().keys({
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      email: Joi.string()
+        .email()
+        .required(),
+    });
+    const { error, value } = Joi.validate(body, schema);
+    if (error && error.details) {
+      return { error };
+    }
+    return { value };
+  },
+};
