8bb582d09f9565ca08f779e298068521b7a31b55
Haider Malik <haidermalik504@gmail.com>
2018-05-04 18:27:01 +0500

    basic setup for client module

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { invoiceRouter } from './resources/invoice';
+import { clientRouter } from './resources/client';
 
 export const restRouter = express.Router();
+restRouter.use('/clients', clientRouter);
 restRouter.use('/invoices', invoiceRouter);
---
+++ src/api/resources/client/client.router.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import clientController from './client.controller';
+
+export const clientRouter = express.Router();
+clientRouter.route('/').post(clientController.create);
