ae37efb990a9d6936481e8c66e7450e056029619
Haider Malik <haidermalik504@gmail.com>
2018-05-09 16:58:00 +0500

    create new user

--- src/api/resources/user/user.controller.js
+++ src/api/resources/user/user.controller.js
@@ -1,5 +1,23 @@
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from 'http-status-codes';
+import userService from './user.service';
+import User from './user.model';
+
 export default {
   async signup(req, res) {
-    return res.json({ msg: 'Signup' });
+    try {
+      // validate the request
+      const { error, value } = userService.validateSchema(req.body);
+      if (error && error.details) {
+        return res.status(BAD_REQUEST).json(error);
+      }
+      // enrypt the user password
+
+      // create new user
+      const user = await User.create(value);
+      return res.json(user);
+    } catch (err) {
+      console.error(err);
+      return res.status(INTERNAL_SERVER_ERROR).json(err);
+    }
   },
 };
---
+++ src/api/resources/user/user.service.js
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+export default {
+  validateSchema(body) {
+    const schema = Joi.object().keys({
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string().required(),
+    });
+    const { error, value } = Joi.validate(body, schema);
+    if (error && error.details) {
+      return { error };
+    }
+    return { value };
+  },
+};
