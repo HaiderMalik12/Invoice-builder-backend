551d429100422b900a71f01611ee35809afc9c2b
Haider Malik <haidermalik504@gmail.com>
2018-05-09 16:49:39 +0500

    basic structure for user module

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { invoiceRouter } from './resources/invoice';
 import { clientRouter } from './resources/client';
+import { userRouter } from './resources/user/user.router';
 
 export const restRouter = express.Router();
 restRouter.use('/clients', clientRouter);
 restRouter.use('/invoices', invoiceRouter);
+restRouter.use('/users', userRouter);
---
+++ src/api/resources/user/user.controller.js
@@ -0,0 +1,5 @@
+export default {
+  async signup(req, res) {
+    return res.json({ msg: 'Signup' });
+  },
+};
