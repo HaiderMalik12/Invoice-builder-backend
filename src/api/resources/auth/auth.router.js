7b54053ba9a03db552275f9f890e3dbd2b15c192
Haider Malik <haidermalik504@gmail.com>
2018-05-13 10:46:35 +0500

    adding routes

--- src/api/resources/auth/auth.router.js
+++ src/api/resources/auth/auth.router.js
@@ -1,6 +1,25 @@
 import express from 'express';
+import passport from 'passport';
 
 export const authRouter = express.Router();
-authRouter.route('/test').get((req, res) => {
-  res.json({ msg: 'working' });
-});
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+authRouter.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login'],
+  })
+);
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+authRouter.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failure' }), (req, res) =>
+  // res.redirect('/');
+  res.json({ msg: 'authenticated' })
+);
