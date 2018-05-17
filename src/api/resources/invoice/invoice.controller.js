2a85bb308fcda3b924fbc017b5752f3a47bacdc6
Haider Malik <haidermalik504@gmail.com>
2018-05-17 10:46:31 +0500

    refactor code into methods

--- src/api/resources/invoice/invoice.controller.js
+++ src/api/resources/invoice/invoice.controller.js
@@ -1,5 +1,5 @@
 import Joi from 'joi';
-import HttpStatus from 'http-status-codes';
+import HttpStatus, { NOT_FOUND } from 'http-status-codes';
 import Invoice from './invoice.model';
 import invoiceService from './invoice.service';
 
@@ -95,117 +95,14 @@ export default {
     try {
       const { id } = req.params;
       const invoice = await Invoice.findById(id).populate('client');
+      if (!invoice) {
+        return res.status(NOT_FOUND).send({ err: 'could not find any invice' });
+      }
       const { subTotal, total } = invoiceService.getTotal(invoice);
-      const templateBody = `
-      <div class="container">
-    <div class="row">
-        <div class="col-xs-6">
-        </div>
-        <div class="col-xs-6 text-right">
-            <h1>INVOICE</h1>
-            <h1>
-                <small>${invoice.item}</small>
-            </h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-5">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4>From:
-                        <a>Jane</a>
-                    </h4>
-                </div>
-                <div class="panel-body">
-                    <p>
-                        jane_doe@gmail.com
-                        <br>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-5 col-xs-offset-2 text-right">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4>To :
-                        <a>${invoice.client.firstName} ${invoice.client.lastName}</a>
-                    </h4>
-                </div>
-                <div class="panel-body">
-                    <p>
-                        ${invoice.client.email}
-                        <br>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>
-                    <h4>Qty</h4>
-                </th>
-                <th>
-                    <h4>Rate</h4>
-                </th>
-                <th>
-                    <h4>Tax</h4>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>${invoice.qty}</td>
-                <td>${invoice.rate}</td>
-                <td>
-                    ${invoice.tax}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <div class="row text-right">
-        <div class="col-xs-2 col-xs-offset-8">
-            <p>
-                <strong>
-                    Sub Total :
-                    <br> TAX :
-                    <br> Total :
-                    <br>
-                </strong>
-            </p>
-        </div>
-        <div class="col-xs-2">
-            <strong>
-                $${subTotal}
-                <br> $${invoice.tax}
-                <br> $${total}
-                <br>
-            </strong>
-        </div>
-    </div>
-</div>
-      `;
-      const html = `
-      <html>
-      <head>
-      <title> Invoice </title>
-      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-       <style>
-       @import url(http://fonts.googleapis.com/css?family=Bree+Serif);
-       body, h1, h2, h3, h4, h5, h6{
-       font-family: 'Bree Serif', serif;
-       }
-       </style>
-      </head>
-
-      <body>
-         ${templateBody}
-      </body>
-      </html>
-      `;
+      const templateBody = invoiceService.getTemplateBody(invoice, subTotal, total);
+      const html = invoiceService.getInvoiceTemplate(templateBody);
       res.pdfFromHTML({
-        filename: 'hello-world.pdf',
+        filename: `${invoice.item}.pdf`,
         htmlContent: html,
       });
     } catch (err) {
