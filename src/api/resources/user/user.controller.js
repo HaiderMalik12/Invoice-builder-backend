516a40ef91f3110a4dd69241ee56263da2faf101
Haider Malik <haidermalik504@gmail.com>
2018-09-12 15:20:54 +0500

    add await

--- src/api/resources/user/user.controller.js
+++ src/api/resources/user/user.controller.js
@@ -1,4 +1,8 @@
-import { BAD_REQUEST, INTERNAL_SERVER_ERROR, UNAUTHORIZED } from 'http-status-codes';
+import {
+	BAD_REQUEST,
+	INTERNAL_SERVER_ERROR,
+	UNAUTHORIZED
+} from 'http-status-codes';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userService from './user.service';
@@ -6,43 +10,45 @@ import User from './user.model';
 import { devConfig } from '../../../config/env/development';
 
 export default {
-  async signup(req, res) {
-    try {
-      const { error, value } = userService.validateSchema(req.body);
-      if (error && error.details) {
-        return res.status(BAD_REQUEST).json(error);
-      }
-      const user = await User.create(value);
-      return res.json({ success: true, message: 'User created successfully' });
-    } catch (err) {
-      console.error(err);
-      return res.status(INTERNAL_SERVER_ERROR).json(err);
-    }
-  },
-  async login(req, res) {
-    try {
-      const { error, value } = userService.validateSchema(req.body);
-      if (error && error.details) {
-        return res.status(BAD_REQUEST).json(error);
-      }
-      const user = await User.findOne({ email: value.email });
-      if (!user) {
-        return res.status(BAD_REQUEST).json({ err: 'invalid email or password' });
-      }
-      const matched = bcryptjs.compare(value.password, user.password);
-      if (!matched) {
-        return res.status(UNAUTHORIZED).json({ err: 'invalid credentials' });
-      }
-      const token = jwt.sign({ id: user._id }, devConfig.secret, {
-        expiresIn: '1d',
-      });
-      return res.json({ success: true, token });
-    } catch (err) {
-      console.error(err);
-      return res.status(INTERNAL_SERVER_ERROR).json(err);
-    }
-  },
-  async test(req, res) {
-    return res.json(req.currentUser);
-  },
+	async signup(req, res) {
+		try {
+			const { error, value } = userService.validateSchema(req.body);
+			if (error && error.details) {
+				return res.status(BAD_REQUEST).json(error);
+			}
+			const user = await User.create(value);
+			return res.json({ success: true, message: 'User created successfully' });
+		} catch (err) {
+			console.error(err);
+			return res.status(INTERNAL_SERVER_ERROR).json(err);
+		}
+	},
+	async login(req, res) {
+		try {
+			const { error, value } = userService.validateSchema(req.body);
+			if (error && error.details) {
+				return res.status(BAD_REQUEST).json(error);
+			}
+			const user = await User.findOne({ email: value.email });
+			if (!user) {
+				return res
+					.status(BAD_REQUEST)
+					.json({ err: 'invalid email or password' });
+			}
+			const matched = await bcryptjs.compare(value.password, user.password);
+			if (!matched) {
+				return res.status(UNAUTHORIZED).json({ err: 'invalid credentials' });
+			}
+			const token = jwt.sign({ id: user._id }, devConfig.secret, {
+				expiresIn: '1d'
+			});
+			return res.json({ success: true, token });
+		} catch (err) {
+			console.error(err);
+			return res.status(INTERNAL_SERVER_ERROR).json(err);
+		}
+	},
+	async test(req, res) {
+		return res.json(req.currentUser);
+	}
 };
